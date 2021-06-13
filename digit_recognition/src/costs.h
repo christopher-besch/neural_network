d8665d5997e10b14ebf3c295fd8f9a259e78e802
Christopher Besch <christopher.besch@gmx.de>
2021-06-13 14:33:35 +0200

    fixed comment

--- digit_recognition/src/costs.h
+++ digit_recognition/src/costs.h
@@ -12,7 +12,8 @@ public:
     // only for test data
     // return cost associated with an output <a> and desired output <y>
     virtual float fn(const arma::fvec& a, const arma::fvec& y) = 0;
-    // return vector of partial derivatives \partial C_x / \partial z of output layer
+    // return vector of partial derivatives \partial C_x / \partial z of output layer times sigmoid_prime(z)
+    // -> error delta from output layer
     // one column per data set
     virtual arma::fmat error(const arma::fmat& z, const arma::fmat& a, const arma::fmat& y) = 0;
 
